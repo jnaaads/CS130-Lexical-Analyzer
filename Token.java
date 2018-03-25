68f939078b8436f6623ff12c50ec80badb031482
jnaaads <36280713+jnaaads@users.noreply.github.com>
2018-03-25 23:49:49 +0800

    Add files via upload

---
+++ Token.java
@@ -0,0 +1,20 @@
+public class Token {
+	public String id;
+	public String lexeme;
+
+	public String getId() {
+		return this.id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getLexeme() {
+		return this.lexeme;
+	}
+
+	public void setLexeme(String lexeme) {
+		this.lexeme = lexeme;
+	}
+}
